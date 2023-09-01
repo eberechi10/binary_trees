40bc6757bf36456861f2b9814794f86957e1fe43
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-08-31 21:56:38 -0700

    updated

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,7 +7,7 @@
  *
  * Return: the created binary tree node, otherwise NULL.
 */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 {
 
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
