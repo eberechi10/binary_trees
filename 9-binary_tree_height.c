46aebb541a96272879c4fd130193c492478375db
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-09-01 04:15:16 -0700

    updated binary_tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,23 +1,46 @@
 #include "binary_trees.h"
 
+
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to root node of the tree to measure
+ * count_height - counts the height of the binary tree.
  *
- * Return: Success gives height of the tree, or 0 if tree is NULL
+ * @tree: tree node
+ * @height: height of the tree.
  *
- * Description: This function calculates the height of a binary tree.The height
- * of a binary tree is the length of the longest path from the root node to a
- * leaf node. The height of an empty tree (tree == NULL) is considered 0.
+ * @mx: the max height
+ */
+void count_height(const binary_tree_t *tree, size_t height, size_t *mx)
+{
+	if (tree)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			if (height > *mx)
+			{
+				*mx = height;
+			}
+		}
+		else
+		{
+			count_height(tree->left, height + 1, mx);
+
+			count_height(tree->right, height + 1, mx);
+		}
+	}
+}
+
+
+/**
+ * binary_tree_height - func to measure the height of binary tree.
+ *
+ * @tree: the root node of the tree.
+ *
+ * Return: if success height, otherwise 0.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t mx_height = 0;
 
-	if (tree == NULL)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	count_height(tree, 0, &mx_height);
+	return (mx_height);
 }
